f3d4a3a7c3d2d04c9d4b0ad17e6c5ed3c08df434
ayosleepy <filippmello2007@gmail.com>
2026-03-02 15:00:03 +0700

    Pract 1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-let product = "Socks";
-
 let app = new Vue({
    el: '#app',
    data: {
-       product: "Socks"
+       product: "Socks",
+       image: "./assets/vmSocks-blue-onWhite.jpg",
+       altText: "A pair of socks"
    }
 })
+
