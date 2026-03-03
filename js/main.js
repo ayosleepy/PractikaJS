699997d4f0a1a0290ec52c7a55446bc83104ea52
ayosleepy <filippmello2007@gmail.com>
2026-03-03 09:43:54 +0700

    Practicheskaya 5

--- js/main.js
+++ js/main.js
@@ -7,20 +7,37 @@ let app = new Vue({
        description: "A pair of warm, fuzzy socks",
        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
        inStock: true,
-       inventory: 100,
        onSale: false,
        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
        variants: [
    {
        variantId: 2234,
-       variantColor: 'green'
+       variantColor: 'green',
+       variantImage: "./assets/vmSocks-green-onWhite.jpg",
    },
    {
        variantId: 2235,
-       variantColor: 'blue'
+       variantColor: 'blue',
+       variantImage: "./assets/vmSocks-blue-onWhite.jpg",
    }
 ],
-       sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
-   }
+       sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+       cart: 0,
+
+   },
+   methods: {
+   addToCart() {
+       this.cart += 1
+   },
+   updateProduct(variantImage) {
+   this.image = variantImage
+   },
+   decreaseCart() {
+    this.cart --
+   },
+
+},
+
 })
+
 
